1550de57d43b9bfd851ccb4e439a56958cb1c002
Joshua <jbatzold530@lsr7.net>
2018-12-11 08:10:12 -0600

    Added boolean expressions and more code for the astronaut, the flappy bird

--- flappybirdjosh.py
+++ flappybirdjosh.py
@@ -1,4 +1,6 @@
 from sense_hat import SenseHat
+from random import randint
+from time import sleep
 
 sense = SenseHat()
 RED = (255, 0, 0)
@@ -26,12 +28,38 @@ def move_pipes(matrix):
         row[-1] = BLUE
     return matrix
 
-matrix = gen pipes(matrix)
+def draw_astronaut(event):
+    global x
+    global y
+    sense.set_pixel(x, y, BLUE)
+    if event.action == "pressed"
+        if event.direction == "up" and y > 0:
+        y -= 1
+        elif event.direction == "down" and y < 7:
+        y += 1
+        elif event.direction == "right" and x < 7:
+        x += 1
+        elif event.direction == "left" and x > 0:
+        x -= 1
+    sense.set_pixel(x, y, YELLOW)
+    
+def check_collision(matrix):
+    if matrix[y][x] == RED:
+        return True
+    else:
+        return False
+sense.stick.direction_any = draw_astronaut
 
-for i in range(9)
-matrix = move_pipes(matrix)
-sense.set_pixels(flatten(matrix))
-sleep(1)
+while True:
+    matrix = gen_pipes(matrix)
+    if check_collision(matrix):
+        break
+    for i in range(3):
+        matrix = move_pipes(matrix)
+        sense.set_pixels(flatten(matrix))
+        sense.set_pixel(x, y, YELLOW)
+        if check_collision(matrix):
+            break
+        sleep(1)
 
-matrix = flatten(matrix)
-sense.set_pixles(matrix)
+
